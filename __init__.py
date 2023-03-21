330be77ceeb29f9282cb69a5e8a49678d221609e
adreasnow <adrea.snow@gmail.com>
2023-03-21 21:46:46 +1100

    initial

---
+++ __init__.py
@@ -0,0 +1,28 @@
+from .db_handler import JobsDB
+from flask import Flask
+import pandas as pd
+import os
+
+app = Flask(__name__)
+if not os.path.exists('/var/www/api/apiApp/jobs.sqlite'):
+    JobsDB(generate=True)
+
+@app.route('/id/<string:job_ID>/<string:status>/<string:cluster>/', methods=['GET', 'POST'])
+def id(job_ID: str, status: str, cluster: str) -> str:
+    with JobsDB() as jobsDB:
+        out = jobsDB.add_job(job_ID, status, cluster)
+    return out
+
+@app.route('/db/', methods=['GET', 'POST'])
+def db() -> str:
+    with JobsDB() as jobsDB:
+        out = jobsDB.query()
+    return out
+
+@app.route('/', methods=['GET', 'POST'])
+def test() -> str:
+    return 'Running!'
+
+if __name__ == '__main__':
+    app.run()
+    
